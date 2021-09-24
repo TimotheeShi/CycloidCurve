204f17c47cc8c572bba5bf3f7eb6185b9776801e
stone729 <15651817889@163.com>
2021-09-24 15:50:43 +0800

    cycloid gear curve

---
+++ CycloidCurve/CycloidGear.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <math.h>
+
+class CycloidGear
+{
+	double CalTheoreticalCurve(double theta) {
+			double x0, y0;
+			x0 = Rz * (sin(theta) - k1 / Zb * sin(Zb * theta));
+			y0 = Rz * (cos(theta) - k1 / Zb * cos(Zb * theta));
+			double c_gamma, s_gamma;
+			double temp = sqrt(1 + k1 * k1 - 2 * k1 * cos(Zb * theta));
+			c_gamma = (k1 * sin(Zb * theta) - sin(theta)) / temp;
+			s_gamma = -(k1 * cos(Zb * theta) + cos(theta)) / temp;
+			double x1, y1;
+			x1 = x0 + rz * c_gamma;
+			y1 = y0 + rz * s_gamma;
+			return sqrt(x1 * x1 + y1 * y1);
+	}
+
+private:
+	double Rz;
+	double Za;
+	double rz;
+	double e;
+
+
+	double Zb = Za + 1.0;
+	double k1 = e * Zb / Rz;
+};
+
